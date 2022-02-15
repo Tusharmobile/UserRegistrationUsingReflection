67a68a56e346c7e59ebf4f2e7458ecbeb3c1bb3c
tushar <tusharshitre123@gmail.com>
2022-02-15 22:07:02 +0530

    add first name program

---
+++ UserRegistrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UserRegistrationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UserRegistrationTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bd8f968-fa25-4bc4-8c60-b9ac678c2adb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- UserRegistrationUsingReflection/UserRegistrationUsingReflection/Program.cs
+++ UserRegistrationUsingReflection/UserRegistrationUsingReflection/Program.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserRegistrationUsingReflection
+namespace UserRegistrationexRefAnn
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome user registration using reflection program");
+            Console.WriteLine("Welcome to User Registration Program");
+            UserRegistration();
             Console.ReadLine();
+        }
+        //Method to perform UserRegistration
+        public static void UserRegistration()
+        {
+            /* UC-1-UserRegistration
+             * Validate Fisrt Name starts with Cap and have atlest 2 letters.
+             */
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(UserRegister.ValidateFirstName(firstName));
         }
     }
 }
