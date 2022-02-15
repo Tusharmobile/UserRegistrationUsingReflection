bae2c75c7e63eaffae0fc43cc3e72d653a8b060b
tushar <tusharshitre123@gmail.com>
2022-02-15 21:52:00 +0530

    add user registration using reflection

---
+++ UserRegistrationUsingReflection/UserRegistrationUsingReflection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome user registration using reflection program");
+            Console.ReadLine();
+        }
+    }
+}
